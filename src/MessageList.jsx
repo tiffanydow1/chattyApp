cd404b8d21761e0399558daeba7fa2f096c3ecf8
Tiffany <tiffanyjdow@gmail.com>
2018-07-10 00:37:55 +0000

    Got first components working

---
+++ src/MessageList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+
+  render() {
+    return(
+    <main className="messages">
+      <Message />
+    </main>
+
+    )
+  }
+}
+
+
+export default MessageList;
